8b34055e7c68be69f819a0213979e39860964b84
Safronova PIE22 <145684166+KateEas@users.noreply.github.com>
2024-06-28 17:29:36 +0700

    Create Header.jsx

---
+++ components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import './Header.css'
+import HeaderTabs from './HeaderTabs';
+import { VscAccount } from "react-icons/vsc";
+
+
+export default function Header({tab, setTab}) {
+  const [now, setNow] = useState(new Date())
+  useEffect(() => {
+    const interval = setInterval(() => setNow(new Date()), 1000)
+
+
+    return () => {
+      clearInterval(interval)
+      console.log('clining')
+    }
+  }, [])
+
+
+  return (
+    <header>
+      {/* <h3>Resalt one</h3> */}
+      <img src='src/img/special img/logo-name.svg' alt='' />
+      <HeaderTabs active={tab} onChange={(current) => setTab(current)}/>
+      <button className='edit-btn'> <VscAccount size='16px' color='#3D4249'/>
+        Личный кабинет
+      </button>
+    </header>
+  )
+}
