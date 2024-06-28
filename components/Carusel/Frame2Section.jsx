0a8cc1558a68f36d3d1183eb045403070d23966b
Safronova PIE22 <145684166+KateEas@users.noreply.github.com>
2024-06-28 17:41:31 +0700

    Add files via upload

---
+++ components/Carusel/Frame2Section.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Carusel.css'
+
+
+export default function Frame2Section() {
+    return (
+        <div className='item'>
+            <div className='item-op'>
+                <h3>Закажи на 65 тыс. в месяц
+                    получи бесплатно пирожных</h3>
+                <p>Или любой другой продукции на 3000руб</p>
+            </div>
+            <div className='item-1'>
+                <img  src='src/img/special img/frame2.svg' />
+            </div>
+
+
+        </div>
+    )
+}
---
+++ components/Carusel/Frame3Section.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Carusel.css'
+
+
+export default function Frame3Section() {
+    return (
+        <section className='item'>
+            <div className='item-op'>
+                <h3> Каждую пятницу 1+1</h3>
+                <p>В соответствии с принципом неопределенности, возмущение плотности синхронизует солитон. Расслоение синхронно.</p>
+            </div>
+            <div className='item-2'> 
+                <img className='img-2 img-3' src='src/img/special img/tort.jpg'/>
+                <img className='img-2' src='src/img/special img/tort.jpg'/>
+            </div>
+        </section>
+    )
+}
