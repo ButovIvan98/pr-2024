a8b770285920d802607a8243d11d885cad66ca75
Safronova PIE22 <145684166+KateEas@users.noreply.github.com>
2024-06-28 17:43:23 +0700

    Create Footer.jsx

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <p>
+            © Кондитерские изделия«Кристалл», 2000–2023
+            </p>
+            <a href="src/documents/politika_konfidencialosti.pdf" target="_blank">Политика конфиденциальности</a> 
+            <a href='tel:+78412709900'>+7 (8412) 709-900</a>
+        </footer>
+    )
+}
