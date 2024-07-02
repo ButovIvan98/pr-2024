96cea86e59f901dc58894aab8404739ae35c544e
Safronova PIE22 <145684166+KateEas@users.noreply.github.com>
2024-07-02 20:03:07 +0700

    Add files via upload

---
+++ navigation/renderErrors.jsx
@@ -0,0 +1,14 @@
+import { useLocation } from "react-router";
+import { HeaderComponent } from "../Header/HeaderComponent";
+
+export const RenderError = () => {
+  const { pathname } = useLocation();
+  return (
+    <div>
+      <HeaderComponent />
+      <h1>Error</h1>
+      <h2>{pathname}</h2>
+      <h3>Такой страницы не существует</h3>
+    </div>
+  );
+};
